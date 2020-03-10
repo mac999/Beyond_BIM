1917254746e86480a0d580b75f8d5acb7b3ae5f7
Tae wook <laputa99999@gmail.com>
2020-03-10 19:23:10 +0900

    Add files via upload

---
+++ BeyondDesign/2.Arduino/connect_processing/connect_processing.ino
@@ -0,0 +1,12 @@
+// Note: This is code for an Arduino board, not Processing
+int sensorPin = 0; // Select input pin
+int val = 0;
+void setup() {
+  Serial.begin(9600); // Open serial port
+}
+void loop() {
+  val = analogRead(sensorPin); // Read value from sensor
+  Serial.write((int)val);  
+  delay(100); // Wait 100 milliseconds
+}
+
